86e094149ee073de4bfe37311a6e0e58b5571aee
Ankit Saraswat <92976285+ANKITSARASWAT12345@users.noreply.github.com>
2026-06-11 23:08:53 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+class Main{
+     public static void main(String[] args){
+        System.out.println("This is Java Progrma");
+        System.out.println("Output from Java Progrma");
+        System.out.println(new Date());
+    }
+}
